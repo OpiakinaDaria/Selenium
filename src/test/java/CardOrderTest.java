1f72ba39e6cda3c207432848edf6efb6115fee47
Дарья Опякина <daryaopyakina@mail.ru>
2022-10-09 22:28:55 +0300

    driver = new ChromeDriver(options)

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -3,34 +3,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
 public class CardOrderTest {
+
+
     private WebDriver driver;
+
+
     @BeforeAll
-    static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
     }
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
         driver = new ChromeDriver();
     }
+
     @AfterEach
-    void tearsDown(){
+    void tearsDown() {
         driver.quit();
         driver = null;
     }
+
     @Test
-    void ShouldCardOrder(){
+    void ShouldCardOrder() {
         driver.get("http://localhost:7777");
         List<WebElement> inputs = driver.findElements(By.tagName("input"));
         inputs.get(0).sendKeys("Булдакова Дарья");
         inputs.get(1).sendKeys("+79113331122");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__text")).click();
-       String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-       String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
-       Assertions.assertEquals(expected, actual);
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText().trim();
+        Assertions.assertEquals(expected, actual);
     }
 }
